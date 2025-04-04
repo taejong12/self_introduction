7d4d1d30c643ccbc3f63622383ddfad3888709e8
TaeJong <dbxowhdsla12@naver.com>
2025-04-04 23:17:07 +0900

    Application 추가

--- self_introduction/src/main/java/com/introduction/web/SelfIntroductionApplication.java
+++ self_introduction/src/main/java/com/introduction/web/SelfIntroductionApplication.java
@@ -2,12 +2,20 @@ package com.introduction.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SelfIntroductionApplication {
+public class SelfIntroductionApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SelfIntroductionApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		System.out.println("#### war 패키징 외장 톰캣 사용 ####");
+		return builder.sources(SelfIntroductionApplication.class);
 	}
 
 }
